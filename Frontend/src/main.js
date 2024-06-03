67230fabfaa41dce548e693db320f6f86e813dc8
Dima Zvazhii <bengorde80@gmail.com>
2024-06-03 07:42:05 +0300

    removed jquery

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -2,14 +2,12 @@
  * Created by chaika on 25.01.16.
  */
 
-$(function(){
+document.addEventListener("DOMContentLoaded", (event) => {
     //This code will execute when the page is ready
-    var PizzaMenu = require('./pizza/PizzaMenu');
-    var PizzaCart = require('./pizza/PizzaCart');
-    var Pizza_List = require('./Pizza_List');
+    const PizzaMenu = require('./pizza/PizzaMenu');
+    const PizzaCart = require('./pizza/PizzaCart');
+    const Pizza_List = require('./Pizza_List');
 
     PizzaCart.initialiseCart();
-    PizzaMenu.initialiseMenu();
-
-
+    PizzaMenu.initialiseMenu();  
 });
