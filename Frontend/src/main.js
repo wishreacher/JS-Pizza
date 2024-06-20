30d674e1d517d0c9e98ec165a1a8980ff2f2db57
klirik <wishreacher@gmail.com>
2024-06-20 21:15:49 +0300

    [WIP] Adding pizza to cart

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(pizza_info => {
             // Select the .menu element
             const menu = document.querySelector('.menu');
+            const cartContent = document.querySelector('.cart-content');
 
             // Loop over the pizza_info array
             pizza_info.forEach(pizza => {
@@ -51,6 +52,47 @@ document.addEventListener('DOMContentLoaded', function() {
                         </div>` : ''}
                     </div>
                 `;
+
+                const orangeButtons = pizzaElement.querySelectorAll('.orange-button');
+
+                orangeButtons.forEach(button => {
+                    button.addEventListener('click', function() {
+                        // Create a new cart item
+                        const cartItem = document.createElement('div');
+                        cartItem.classList.add('cart-item');
+                        cartItem.classList.add('flex');
+                        cartItem.innerHTML = `
+                    <div class="cart-item-description">
+                        <p class="order-name">${pizza.title} (${pizza.small_size.size})</p>
+                        <div class="order-params flex">
+                            <div class="radius flex">
+                                <img src="assets/images/size-icon.svg" alt="">
+                                <p class="icon-text">${pizza.small_size.size}</p>
+                            </div>
+                            <div class="weight flex">
+                                <img src="assets/images/weight.svg" alt="">
+                                <p class="icon-text">${pizza.small_size.weight}</p>
+                            </div>
+                        </div>
+                        <div class="order-controls flex">
+                            <p class="order-price">${pizza.small_size.price}грн</p>
+                            <div class="order-amount-container">
+                                <button class="round-button minus-button">-</button>
+                                <p class="order-amount">1</p>
+                                <button class="round-button plus-button">+</button>
+                            </div>
+                            <button class="round-button cross-button">x</button>
+                        </div>
+                    </div>
+                    <div class="cart-item-image-container">
+                        <img src="${pizza.icon}" alt="pizza" class="cart-item-image">
+                    </div>
+                `;
+
+                        // Append the new cart item to the cart content
+                        cartContent.appendChild(cartItem);
+                    });
+                });
 
                 // Append the .pizza element to the .menu element
                 menu.appendChild(pizzaElement);
