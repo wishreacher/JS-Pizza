ae03cccd38f5ff49e1dcba0da01997cb494e9f40
klirik <wishreacher@gmail.com>
2024-06-20 20:38:07 +0300

    Added initial pizza load from json

--- Frontend/src/Pizza_List.js
+++ Frontend/src/Pizza_List.js
@@ -1,11 +1,10 @@
 /**
  * Created by diana on 12.01.16.
  */
-
-var pizza_info = [
+const pizza_info = [
     {
-        id:1,
-        icon:'assets/images/pizza_7.jpg',
+        id: 1,
+        icon: 'assets/images/pizza_7.jpg',
         title: "Імпреза",
         type: 'М’ясна піца',
         content: {
@@ -15,23 +14,23 @@ var pizza_info = [
             pineapple: ['ананаси'],
             additional: ['томатна паста', 'петрушка']
         },
-        small_size:{
+        small_size: {
             weight: 370,
             size: 30,
             price: 99
         },
-        big_size:{
+        big_size: {
             weight: 660,
             size: 40,
             price: 169
         },
-        is_new:true,
-        is_popular:true
+        is_new: true,
+        is_popular: true
 
     },
     {
-        id:2,
-        icon:'assets/images/pizza_2.jpg',
+        id: 2,
+        icon: 'assets/images/pizza_2.jpg',
         title: "BBQ",
         type: 'М’ясна піца',
         content: {
@@ -40,21 +39,21 @@ var pizza_info = [
             mushroom: ['шампінйони'],
             additional: ['петрушка', 'оливки']
         },
-        small_size:{
+        small_size: {
             weight: 460,
             size: 30,
             price: 139
         },
-        big_size:{
+        big_size: {
             weight: 840,
             size: 40,
             price: 199
         },
-        is_popular:true
+        is_popular: true
     },
     {
-        id:3,
-        icon:'assets/images/pizza_1.jpg',
+        id: 3,
+        icon: 'assets/images/pizza_1.jpg',
         title: "Міксовий поло",
         type: 'М’ясна піца',
         content: {
@@ -63,42 +62,42 @@ var pizza_info = [
             pineapple: ['ананаси'],
             additional: ['кукурудза', 'петрушка', 'соус томатний']
         },
-        small_size:{
+        small_size: {
             weight: 430,
             size: 30,
             price: 115
         },
-        big_size:{
+        big_size: {
             weight: 780,
             size: 40,
             price: 179
         }
     },
     {
-        id:4,
-        icon:'assets/images/pizza_5.jpg',
+        id: 4,
+        icon: 'assets/images/pizza_5.jpg',
         title: "Сициліано",
         type: 'М’ясна піца',
         content: {
             meat: ['вітчина', 'салямі'],
             cheese: ['сир моцарелла'],
             mushroom: ['шампінйони'],
-            additional: ['перець болгарський',  'соус томатний']
+            additional: ['перець болгарський', 'соус томатний']
         },
-        small_size:{
+        small_size: {
             weight: 450,
             size: 30,
             price: 111
         },
-        big_size:{
+        big_size: {
             weight: 790,
             size: 40,
             price: 169
         }
     },
     {
-        id:17,
-        icon:'assets/images/pizza_3.jpg',
+        id: 17,
+        icon: 'assets/images/pizza_3.jpg',
         title: "Маргарита",
         type: 'Вега піца',
         content: {
@@ -106,15 +105,15 @@ var pizza_info = [
             tomato: ['помідори'],
             additional: ['базилік', 'оливкова олія', 'соус томатний']
         },
-        small_size:{
+        small_size: {
             weight: 370,
             size: 30,
             price: 89
         }
     },
     {
-        id:43,
-        icon:'assets/images/pizza_6.jpg',
+        id: 43,
+        icon: 'assets/images/pizza_6.jpg',
         title: "Мікс смаків",
         type: 'М’ясна піца',
         content: {
@@ -124,20 +123,20 @@ var pizza_info = [
             pineapple: ['ананаси'],
             additional: ['цибуля кримська', 'огірки квашені', 'соус гірчичний']
         },
-        small_size:{
+        small_size: {
             weight: 470,
             size: 30,
             price: 115
         },
-        big_size:{
+        big_size: {
             weight: 780,
             size: 40,
             price: 180
         }
     },
     {
-        id:90,
-        icon:'assets/images/pizza_8.jpg',
+        id: 90,
+        icon: 'assets/images/pizza_8.jpg',
         title: "Дольче Маре",
         type: 'Морська піца',
         content: {
@@ -145,15 +144,15 @@ var pizza_info = [
             cheese: ['сир моцарелла'],
             additional: ['оливкова олія', 'вершки']
         },
-        big_size:{
+        big_size: {
             weight: 845,
             size: 40,
             price: 399
         }
     },
     {
-        id:6,
-        icon:'assets/images/pizza_4.jpg',
+        id: 6,
+        icon: 'assets/images/pizza_4.jpg',
         title: "Россо Густо",
         type: 'Морська піца',
         content: {
@@ -161,12 +160,12 @@ var pizza_info = [
             cheese: ['сир моцарелла'],
             additional: ['оливкова олія', 'вершки']
         },
-        small_size:{
+        small_size: {
             weight: 400,
             size: 30,
             price: 189
         },
-        big_size:{
+        big_size: {
             weight: 700,
             size: 40,
             price: 299
@@ -174,4 +173,4 @@ var pizza_info = [
     }
 ];
 
-module.exports = pizza_info;
+export {pizza_info}
--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,13 +1,59 @@
-/**
- * Created by chaika on 25.01.16.
- */
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('../src/Pizza_List.json')
+        .then(response => response.json())
+        .then(pizza_info => {
+            // Select the .menu element
+            const menu = document.querySelector('.menu');
 
-document.addEventListener("DOMContentLoaded", (event) => {
-    //This code will execute when the page is ready
-    const PizzaMenu = require('./pizza/PizzaMenu');
-    const PizzaCart = require('./pizza/PizzaCart');
-    const Pizza_List = require('./Pizza_List');
+            // Loop over the pizza_info array
+            pizza_info.forEach(pizza => {
+                // Create a new .pizza element
+                const pizzaElement = document.createElement('div');
+                pizzaElement.classList.add('pizza');
 
-    PizzaCart.initialiseCart();
-    PizzaMenu.initialiseMenu();  
+                // Fill the .pizza element with the appropriate data
+                pizzaElement.innerHTML = `
+                    <img src="${pizza.icon}" alt="pizza" class="pizza-image">
+                    <p class="pizza-name">${pizza.title}</p>
+                    <p class="pizza-style">${pizza.type}</p>
+                    <p class="pizza-description">${Object.values(pizza.content).flat().join(', ')}</p>
+                    <div class="sizes-container">
+                        <div class="size">
+                            <div class="radius flex">
+                                <img src="assets/images/size-icon.svg" alt="">
+                                <p class="icon-text">${pizza.small_size.size}</p>
+                            </div>
+                            <div class="weight flex">
+                                <img src="assets/images/weight.svg" alt="">
+                                <p class="icon-text">${pizza.small_size.weight}</p>
+                            </div>
+                            <div class="price-container">
+                                <p class="price-tag">${pizza.small_size.price}</p>
+                                <p class="currency">грн.</p>
+                            </div>
+                            <button class="orange-button">Купити</button>
+                        </div>
+                        ${pizza.big_size ? `
+                        <div class="size">
+                            <div class="radius flex">
+                                <img src="assets/images/size-icon.svg" alt="">
+                                <p class="icon-text">${pizza.big_size.size}</p>
+                            </div>
+                            <div class="weight flex">
+                                <img src="assets/images/weight.svg" alt="">
+                                <p class="icon-text">${pizza.big_size.weight}</p>
+                            </div>
+                            <div class="price-container">
+                                <p class="price-tag">${pizza.big_size.price}</p>
+                                <p class="currency">грн.</p>
+                            </div>
+                            <button class="orange-button">Купити</button>
+                        </div>` : ''}
+                    </div>
+                `;
+
+                // Append the .pizza element to the .menu element
+                menu.appendChild(pizzaElement);
+            });
+        });
 });
